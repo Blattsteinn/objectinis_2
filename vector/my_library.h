8873106db5eb5e0c8f4887ee8d85e05f063b1ce8
Blattstein <arnas.lukosius@outlook.com>
2025-05-04 16:42:22 +0300

    Update my_library.h

--- vector/my_library.h
+++ vector/my_library.h
@@ -5,6 +5,12 @@
 
 #include <limits>  // for std::numeric_limits
 
+#include <algorithm>   // for std::move_backward, std::copy
+#include <iterator>    // for std::distance
+#include <type_traits> // for std::enable_if_t, std::is_integral
+#include <ranges>
+
+
 using std::cout;
 using std::endl;
 using std::swap;
