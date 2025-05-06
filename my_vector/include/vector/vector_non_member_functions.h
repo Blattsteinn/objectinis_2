4dd90945784fe183c655998b187b84d8235082dd
Blattstein <arnas.lukosius@outlook.com>
2025-05-06 12:30:41 +0300

    Create vector_non_member_functions.h

---
+++ my_vector/include/vector/vector_non_member_functions.h
@@ -0,0 +1,40 @@
+
+template <typename T>
+Vector<T>::bool operator==(const Vector& other) const {
+    if (size_ != other.size_) return false;
+
+    return std::equal(begin(), end(), 
+              other.begin(), other.end());
+ }
+
+template <typename T>
+typename Vector<T>::bool operator!=(const Vector& other) const {
+    return !(other == *this);
+ }
+
+template <typename T>
+typename Vector<T>::bool operator>(const Vector& other) const {
+    // true if the first range is lexicographically less than the second, otherwise false.  
+    // Note: always uses < (so it's -  other < this)
+   return std::lexicographical_compare(other.begin(), other.end(),   // first range  (other)
+                                       begin(), end());              // second range (*this)
+ }
+
+template <typename T>
+typename Vector<T>::bool operator>=(const Vector& other) const {
+    return !std::lexicographical_compare(begin(),      end(),
+                                         other.begin(), other.end()
+    );
+}
+
+template <typename T>
+typename Vector<T>::bool operator<(const Vector& other) const {
+   return std::lexicographical_compare(begin(), end(), 
+                                       other.begin(), other.end());
+ }
+
+template <typename T>
+typename Vector<T>::bool operator<=(const Vector& other) const {
+    return !std::lexicographical_compare(other.begin(), other.end(),
+                                         begin(), end());
+}
